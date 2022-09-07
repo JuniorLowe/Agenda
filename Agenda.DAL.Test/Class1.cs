ee5ea4f0ac2866ea290971d70ecc8869eab4b05a
JuniorLowe <juniorlowe.ti@gmail.com>
2022-09-07 15:23:58 -0300

    Montagem do Escopo de testes

---
+++ Agenda.DAL.Test/Class1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Agenda.DAL.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
